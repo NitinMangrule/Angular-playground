61d55482339434e6744609f204cf4cb3f9f771f0
NitinMangrule <ndmangrule@gmail.com>
2020-12-13 17:14:39 +0530

    5 files modified, 3 added, 1 deleted

---
+++ src/app/card/card.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: "Card",
+  templateUrl: "./card.component.html",
+  styleUrls: ["./card.component.css"]
+})
+export class CardComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
