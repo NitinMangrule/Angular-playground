f0675eaab9d8ecc728107b87f0ac34e88bf72b57
NitinMangrule <ndmangrule@gmail.com>
2020-12-13 17:32:05 +0530

    5 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,37 @@ import { Component, VERSION } from "@angular/core";
 })
 export class AppComponent {
   name = "Angular " + VERSION.major;
+  title = "Burger King";
+  dishData = [
+    {
+      title: "Burger",
+      imgPath: "https://foodish-api.herokuapp.com/images/burger/burger48.jpg",
+      description:
+        "The Best Burger Recipe! Sharing our secrets for making restaurant quality burgers, from the juiciest burger patties to the best buns and burger"
+    },
+    {
+      title: "Biryani",
+      imgPath: "https://foodish-api.herokuapp.com/images/biryani/biryani9.jpg",
+      description:
+        "The Best Burger Recipe! Sharing our secrets for making restaurant quality burgers, from the juiciest burger patties to the best buns and burger"
+    },
+    {
+      title: "Dosa",
+      imgPath: "https://foodish-api.herokuapp.com/images/dosa/dosa67.jpg",
+      description:
+        "The Best Burger Recipe! Sharing our secrets for making restaurant quality burgers, from the juiciest burger patties to the best buns and burger"
+    },
+    {
+      title: "Idly",
+      imgPath: "https://foodish-api.herokuapp.com/images/idly/idly47.jpg",
+      description:
+        "The Best Burger Recipe! Sharing our secrets for making restaurant quality burgers, from the juiciest burger patties to the best buns and burger"
+    },
+    {
+      title: "Pizza",
+      imgPath: "https://foodish-api.herokuapp.com/images/pizza/pizza30.jpg",
+      description:
+        "The Best Burger Recipe! Sharing our secrets for making restaurant quality burgers, from the juiciest burger patties to the best buns and burger"
+    }
+  ];
 }
--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, Input, OnInit } from "@angular/core";
 
 @Component({
   selector: "Card",
@@ -7,6 +7,9 @@ import { Component, OnInit } from "@angular/core";
 })
 export class CardComponent implements OnInit {
   constructor() {}
+  @Input() title: string;
+  @Input() path: string;
+  @Input() desc: string;
 
   ngOnInit() {}
 }
